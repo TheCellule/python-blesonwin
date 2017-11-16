8aa2c286903cf2a84a723ae7ed51b1ea74e1c1da
Wayne Keenan <wayne@cannybots.com>
2017-11-16 16:57:45 +0000

    passing handful of advertisement report fields

--- blesonwin/blesonwin.cpp
+++ blesonwin/blesonwin.cpp
@@ -1,36 +1,39 @@
 // blesonwin - Native Python Module for accessing the Windows 10+ BLuetooth LE Functions
+
 // wayne@thebubbleworks.com
 
 // EXPERIMENTAL
 
-// This was startred life as the MS C++ Extentions for Python Example, then C++/WinRT calls were added.
+// This startred life as the MS C++ Extentions for Python Example, then C++/WinRT library and CPython calls were added.
 // See: https://docs.microsoft.com/en-gb/visualstudio/python/cpp-and-python
 // See: https://github.com/Microsoft/cppwinrt
 
-// From standard example perform these VS2017 project changes changes for C++/WinRT:
+// From the standard example perform these VS2017 project changes changes for C++/WinRT:
 
-// On the C++ project item in Solution Explorer:
+// On the C++ 'blesonwin' project item in Solution Explorer:
 // add Properties -> C/C++ -> Additional Include paths: $(ProjectDir)/winrt
-// add Properties -> C/C++ -> Commandline -> Extra Flags : /await /std:c++latest 
 // add Properties -> C/C++, General tab,  Consume Windows Runtime Extension to Yes (/ZW). 
-// add Properties -> C/C++, Code Generation,  Disable Minmal Rebuild 
-// add Properties -> C/C++, General tab, Additional Using paths: 
-//							C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcpackages
-//							C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\lib\store\references
-//							C:\Program Files (x86)\Windows Kits\10\UnionMetadata
-// TODO: use macros, e.g. $(VCInstallDir), $(WindowsSDKDir)
+// Project Properties -> C/C++ -> CommandLine -> Extra Flags : /await /std:c++latest 
+// Project Properties -> C/C++ -> General -> Additional Include paths: $(ProjectDir)/winrt
+// Project Properties -> C/C++ -> General -> Additional Using paths: $(VCInstallDir)vcpackages;$(VCInstallDir)lib\store\references;$(WindowsSDKDir)UnionMetadata
+// Copy winrt folder to project root as '$(ProjectDir)/winrt' from https://github.com/Microsoft/cppwinrt/tree/master/10.0.16299.0/winrt
 
-// Not currently needed on Using path:
+
+
+// Not currently needed on the 'Additional Using' path:
 // C:\Program Files (x86)\Windows Kits\10\References\Windows.Networking.Connectivity.WwanContract\1.0.0.0
 // C:\Program Files (x86)\Windows Kits\10\References\Windows.Foundation.UniversalApiContract\1.0.0.0
 // C:\Program Files (x86)\Windows Kits\10\References\Windows.Foundation.FoundationContract\1.0.0.0
 // see: https://social.msdn.microsoft.com/Forums/sqlserver/en-US/a6769b36-0390-44c7-93d9-428298a5234d/setting-path-for-windowswinmd-in-visual-studio-2015-when-using-zw-option-to-write-ccx-code-to?forum=visualstudiogeneral
 
-
+// Taken care of by the /ZW flag set above.
 //#pragma comment(lib, "windowsapp")
 
 #include <Windows.h>
+#include <string>
 #include <iostream>
+#include <sstream> 
+#include <iomanip>
 
 #include "winrt/Windows.Foundation.h"
 #include "winrt/Windows.Devices.Bluetooth.h"
@@ -44,39 +47,39 @@ using namespace winrt::Windows::Devices;
 using namespace winrt::Windows::Devices::Bluetooth;
 
 
-void call_on_advertisement_callback(const Bluetooth::Advertisement::BluetoothLEAdvertisementReceivedEventArgs advertisement);
-
+void call_on_advertisement_callback(const Bluetooth::Advertisement::BluetoothLEAdvertisementReceivedEventArgs advertisementRecvEvent);
+std::wstring formatBluetoothAddress(unsigned long long BluetoothAddress);
 
 Bluetooth::Advertisement::BluetoothLEAdvertisementWatcher bleAdvertisementWatcher = nullptr;
 Bluetooth::Advertisement::BluetoothLEAdvertisementPublisher bleAdvertisementPublisher = nullptr;
 
 
 PyObject* initialise_impl() {
-	cout << "Initiliasing C++/WinRT" << endl;
+	//cout << "Initiliasing C++/WinRT" << endl;
 	winrt::init_apartment();
 
 	Py_RETURN_NONE;
 }
 
 PyObject* start_observer_impl() {
-	cout << "Creating AdvertisementWatcher" << endl;
+	//cout << "Creating AdvertisementWatcher" << endl;
 	bleAdvertisementWatcher = Bluetooth::Advertisement::BluetoothLEAdvertisementWatcher();
 
 	bleAdvertisementWatcher.ScanningMode(Bluetooth::Advertisement::BluetoothLEScanningMode::Active);
 
 	bleAdvertisementWatcher.Received([=](const Bluetooth::Advertisement::BluetoothLEAdvertisementWatcher watcher, const Bluetooth::Advertisement::BluetoothLEAdvertisementReceivedEventArgs advertisement) {
-		cout << "Advertisment Report received..."  <<endl;
+		//cout << "Advertisment Report received..."  <<endl;
 		call_on_advertisement_callback(advertisement);
 	});
 
-	cout << "Starting AdvertisementWatcher" << endl;
+	//cout << "Starting AdvertisementWatcher" << endl;
 	bleAdvertisementWatcher.Start();
 
 	Py_RETURN_NONE;
 }
 
 PyObject* stop_observer_impl() {
-	cout << "Stopping AdvertisementWatcher" << endl;
+	//cout << "Stopping AdvertisementWatcher" << endl;
 	bleAdvertisementWatcher.Stop();
 
 	Py_RETURN_NONE;
@@ -128,10 +131,21 @@ static PyObject* on_advertisement_impl(PyObject *dummy, PyObject *args)
 	return result;
 }
 
-void call_on_advertisement_callback(const Bluetooth::Advertisement::BluetoothLEAdvertisementReceivedEventArgs advertisement) {
+// Passed : https://docs.microsoft.com/en-us/uwp/api/windows.devices.bluetooth.advertisement.bluetoothleadvertisementreceivedeventargs
+// (which is NOT an Advertising Report, the ADvReport is a member of the Event, and is documented here:
+// https://docs.microsoft.com/en-us/uwp/api/windows.devices.bluetooth.advertisement.bluetoothleadvertisement
+
+void call_on_advertisement_callback(const Bluetooth::Advertisement::BluetoothLEAdvertisementReceivedEventArgs advertisementRecvEvent) {
 	if (!on_advertisement_callback) {
 		return;
 	}
+	int rssi = advertisementRecvEvent.RawSignalStrengthInDBm();
+	auto advType = advertisementRecvEvent.AdvertisementType();
+	auto bdaddr = formatBluetoothAddress(advertisementRecvEvent.BluetoothAddress()).c_str();
+	auto advertisement = advertisementRecvEvent.Advertisement();
+	auto localName = advertisement.LocalName().c_str();
+	auto serviceUUIDs = advertisement.ServiceUuids();	// aggregates all 16, 32 & 128 bit UUID's into this list!
+
 	PyGILState_STATE gstate;
 	gstate = PyGILState_Ensure();
 
@@ -140,7 +154,11 @@ void call_on_advertisement_callback(const Bluetooth::Advertisement::BluetoothLEA
 
 	PyObject *arglist;
 	//arglist = Py_BuildValue("(o)", advertisement_dict);
-	arglist = Py_BuildValue("(i)", advertisement.RawSignalStrengthInDBm());
+	arglist = Py_BuildValue("({s:i,s:s,s:s})", 
+		"RSSI", rssi,
+		"ADDRESS", bdaddr,
+		"LOCALNAME", localName
+		);
 
 	PyObject *result;
 	result = PyObject_CallObject(on_advertisement_callback, arglist);
@@ -183,4 +201,24 @@ PyMODINIT_FUNC PyInit_blesonwin() {
 		PyEval_InitThreads();
 	}
 	return PyModule_Create(&blesonwin_module);
+}
+
+
+// Utils
+
+// from : https://github.com/urish/win-ble-cpp/blob/master/BLEScanner/BLEScanner.cpp#L32
+std::wstring formatBluetoothAddress(unsigned long long BluetoothAddress) {
+	cout << BluetoothAddress << endl;
+	std::wostringstream ret;
+	ret << std::hex << std::setfill(L'0')
+		<< std::setw(2) << ((BluetoothAddress >> (5 * 8)) & 0xff) << ":"
+		<< std::setw(2) << ((BluetoothAddress >> (4 * 8)) & 0xff) << ":"
+		<< std::setw(2) << ((BluetoothAddress >> (3 * 8)) & 0xff) << ":"
+		<< std::setw(2) << ((BluetoothAddress >> (2 * 8)) & 0xff) << ":"
+		<< std::setw(2) << ((BluetoothAddress >> (1 * 8)) & 0xff) << ":"
+		<< std::setw(2) << ((BluetoothAddress >> (0 * 8)) & 0xff);
+
+	cout << ret.str().c_str() << endl;
+
+	return ret.str();
 }
