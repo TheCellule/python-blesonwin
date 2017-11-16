de05705bcfb299d3ad69f6cf70de415d4855e3de
Wayne Keenan <wayne@cannybots.com>
2017-11-16 04:33:06 +0000

    remove example code and tidyup

--- blesonwin/blesonwin.cpp
+++ blesonwin/blesonwin.cpp
@@ -1,28 +1,33 @@
-// This is the MS C++ Extentions for P{ython Example with with added C++/WinRT calls
+// blesonwin - Native Python Module for accessing the Windows 10+ BLuetooth LE Functions
+// wayne@thebubbleworks.com
 
-// FRom standard example perfomr these VS2017 project changes changes:
+// EXPERIMENTAL
+
+// This was startred life as the MS C++ Extentions for Python Example, then C++/WinRT calls were added.
+// See: https://docs.microsoft.com/en-gb/visualstudio/python/cpp-and-python
+// See: https://github.com/Microsoft/cppwinrt
+
+// From standard example perform these VS2017 project changes changes for C++/WinRT:
 
 // add winrt include. $(ProjectDir)/winrt
 // add Properties -> C/C++ -> Commandline -> Extra Flags : /await /std:c++latest 
 // add Properties -> C/C++, General tab,  Consume Windows Runtime Extension to Yes (/ZW). 
-// add Properties -> C/C++, Code GEneration,  Disable Minmal Rebuild 
-//						This enables component extensions (C++/CX).
-// add Properties -> C/C++, General tab, Addiotnal Using paths: 
-// C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcpackages
-// C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\lib\store\references
-// C:\Program Files (x86)\Windows Kits\10\UnionMetadata
+// add Properties -> C/C++, Code Generation,  Disable Minmal Rebuild 
+// add Properties -> C/C++, General tab, Additional Using paths: 
+//							C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcpackages
+//							C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\lib\store\references
+//							C:\Program Files (x86)\Windows Kits\10\UnionMetadata
 
-/* see: https://social.msdn.microsoft.com/Forums/sqlserver/en-US/a6769b36-0390-44c7-93d9-428298a5234d/setting-path-for-windowswinmd-in-visual-studio-2015-when-using-zw-option-to-write-ccx-code-to?forum=visualstudiogeneral
-C:\Program Files (x86)\Windows Kits\10\UnionMetadata
-C:\Program Files (x86)\Windows Kits\10\References\Windows.Networking.Connectivity.WwanContract\1.0.0.0
-C:\Program Files (x86)\Windows Kits\10\References\Windows.Foundation.UniversalApiContract\1.0.0.0
-C:\Program Files (x86)\Windows Kits\10\References\Windows.Foundation.FoundationContract\1.0.0.0
-*/
+// Not currently needed on Using path:
+// C:\Program Files (x86)\Windows Kits\10\References\Windows.Networking.Connectivity.WwanContract\1.0.0.0
+// C:\Program Files (x86)\Windows Kits\10\References\Windows.Foundation.UniversalApiContract\1.0.0.0
+// C:\Program Files (x86)\Windows Kits\10\References\Windows.Foundation.FoundationContract\1.0.0.0
+// see: https://social.msdn.microsoft.com/Forums/sqlserver/en-US/a6769b36-0390-44c7-93d9-428298a5234d/setting-path-for-windowswinmd-in-visual-studio-2015-when-using-zw-option-to-write-ccx-code-to?forum=visualstudiogeneral
+
 
 //#pragma comment(lib, "windowsapp")
 
 #include <Windows.h>
-#include <cmath>    
 #include <iostream>
 
 #include "winrt/Windows.Foundation.h"
@@ -31,58 +36,49 @@ C:\Program Files (x86)\Windows Kits\10\References\Windows.Foundation.FoundationC
 
 #include <Python.h>
 
-#include "winrt/Windows.Foundation.h"
-#include "winrt/Windows.Web.Syndication.h"
-using namespace winrt;
-using namespace winrt::Windows::Foundation;
-using namespace winrt::Windows::Web::Syndication;
 using namespace std;
-
 using namespace winrt::Windows::Foundation;
 using namespace winrt::Windows::Devices;
 
-const double e = 2.7182818284590452353602874713527;
 
-double sinh_impl(double x) {
-	return (1 - pow(e, (-2 * x))) / (2 * pow(e, -x));
-}
-
-double cosh_impl(double x) {
-	return (1 + pow(e, (-2 * x))) / (2 * pow(e, -x));
-}
+Bluetooth::Advertisement::BluetoothLEAdvertisementWatcher bleAdvertisementWatcher = nullptr;
 
 
-PyObject* tanh_impl(PyObject *, PyObject* o) {
-	double x = PyFloat_AsDouble(o);
-	double tanh_x = sinh_impl(x) / cosh_impl(x);
-
-	cout << "Calling WinRT" << endl;
-
-	// see: https://github.com/Microsoft/cppwinrt/issues/53
-	init_apartment();
+PyObject* initialise_impl() {
+	cout << "Initiliasing C++/WinRT" << endl;
+	winrt::init_apartment();
 
 	cout << "Creating AdvertisementWatcher" << endl;
+	bleAdvertisementWatcher = Bluetooth::Advertisement::BluetoothLEAdvertisementWatcher();
 
-	Bluetooth::Advertisement::BluetoothLEAdvertisementWatcher bleAdvertisementWatcher =  Bluetooth::Advertisement::BluetoothLEAdvertisementWatcher();
+	Py_RETURN_NONE;
+}
 
-	//cout << "Starting AdvertisementWatcher" << endl;
-	//bleAdvertisementWatcher.Start();
+PyObject* start_observer_impl() {
+	cout << "Starting AdvertisementWatcher" << endl;
+	bleAdvertisementWatcher.Start();
 
-	return PyFloat_FromDouble(tanh_x);
+	Py_RETURN_NONE;
+}
+
+PyObject* stop_observer_impl() {
+	cout << "Stopping AdvertisementWatcher" << endl;
+	bleAdvertisementWatcher.Stop();
+
+	Py_RETURN_NONE;
 }
 
 static PyMethodDef blesonwin_methods[] = {
-	// The first property is the name exposed to python, the second is the C++ function name        
-	{ "fast_tanh", (PyCFunction)tanh_impl, METH_O, nullptr },
-
-	// Terminate the array with an object containing nulls.
+	{ "initialise", (PyCFunction)initialise_impl, METH_NOARGS, nullptr },
+	{ "start_observer", (PyCFunction)start_observer_impl, METH_NOARGS, nullptr },
+	{ "stop_observer", (PyCFunction)stop_observer_impl, METH_NOARGS, nullptr },
 	{ nullptr, nullptr, 0, nullptr }
 };
 
 static PyModuleDef blesonwin_module = {
 	PyModuleDef_HEAD_INIT,
 	"blesonwin",                        // Module name
-	"Provides some functions, but faster",  // Module description
+	"Provides a BluetoothLE Wrapper",	// Module description
 	0,
 	blesonwin_methods                   // Structure that defines the methods
 };
